41929ebc2f9bbde113de89b10b90cb63c83283ae
MuhmmadShakeel <mshakiroy@gmail.com>
2026-08-03 16:27:52 +0500

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './componenets/common/Navbar'
+import HomePages from './pages/HomePages'
+import Footer from './componenets/common/Footer'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <HomePages/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/HomePages.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../componenets/home/Hero'
+
+function HomePages() {
+  return (
+    <div>
+      <Hero/>
+    </div>
+  )
+}
+
+export default HomePages
